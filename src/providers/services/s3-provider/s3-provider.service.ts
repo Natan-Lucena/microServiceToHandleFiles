290359151e826cd2ecc3a714a51835f7a991586e
Natan Lucena <zerogamerptbr@gmail.com>
2024-06-18 11:39:27 -0300

    feat: add S3 provider

---
+++ src/providers/services/s3-provider/s3-provider.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
+
+import 'dotenv/config';
+
+interface IUploadImage {
+  file: Express.Multer.File;
+  userId: number;
+}
+
+@Injectable()
+export class S3ProviderService {
+  AWS_S3_BUCKET = process.env.AWS_S3_BUCKET;
+  s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+
+  async uploadFile({ file, userId }: IUploadImage) {
+    const params = {
+      Bucket: this.AWS_S3_BUCKET,
+      Key: String(userId) + Date.now().toString(),
+      Body: file.buffer,
+      ACL: 'public-read',
+      ContentType: file.mimetype,
+      ContentDisposition: 'inline',
+      CreateBucketConfiguration: {
+        LocationConstraint: process.env.AWS_REGION,
+      },
+    };
+    return await this.s3.upload(params).promise();
+  }
+
+  async getFileFromUrl(url: string): Promise<Buffer> {
+    const key = url.split('/').slice(-1)[0];
+
+    const params = {
+      Bucket: this.AWS_S3_BUCKET,
+      Key: key,
+    };
+
+    try {
+      const data = await this.s3.getObject(params).promise();
+      return data.Body as Buffer;
+    } catch (error) {
+      throw new NotFoundException('Arquivo não encontrado.');
+    }
+  }
+}
